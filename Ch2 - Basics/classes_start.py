0a207d678820a6d6b55ae4e73671fec70922ef31
Shibin <39266464+shibinj@users.noreply.github.com>
2022-08-17 14:23:56 +0530

    Updated

--- Ch2 - Basics/classes_start.py
+++ Ch2 - Basics/classes_start.py
@@ -3,3 +3,47 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+class vehicle():
+    def __init__(self, bodystyle):
+        self.bodystyle = bodystyle
+
+    def drive(self, speed):
+        self.mode = "driving"
+        self.speed = speed
+
+class car(vehicle):
+    def __init__(self, enginetype):
+        super().__init__("car")
+        self.wheels = 4
+        self.doors = 4
+        self.enginetype = enginetype
+
+    def drive(self,speed):
+        super().drive(speed)
+        print("Driving my", self.enginetype, "car at", self.speed)
+
+class Motorcycle(vehicle):
+    def __init__(self, enginetype, hassidecar):
+        super().__init__("Motorcycle")
+        if (hassidecar):
+            self.wheels = 3
+        else:
+            self.wheels = 2
+        self.doors = 0
+        self.enginetype = enginetype
+        
+    def drive(self,speed):
+        super().drive(speed)
+        print("Driving my", self.enginetype, "motorcycle at", self.speed)
+
+car1 = car("gas")
+car2 = car("electric")
+mc1 = Motorcycle("gas", True)
+
+print(mc1.wheels)
+print(car1.enginetype)
+print(car2.doors)
+
+car1.drive(30)
+car2.drive(40)
+mc1.drive(50)
